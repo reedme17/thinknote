ce7168301471914affa785de40c9a4708f3d52ec
ty <ting.middle.yan@gmail.com>
2026-04-05 14:35:37 -0400

    Initial Commit

---
+++ thinknoteTests/thinknoteTests.swift
@@ -0,0 +1,19 @@
+//
+//  thinknoteTests.swift
+//  thinknoteTests
+//
+//  Created by 严汀 on 4/5/26.
+//
+
+import Testing
+@testable import thinknote
+
+struct thinknoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
